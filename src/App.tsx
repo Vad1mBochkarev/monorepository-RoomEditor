75ec499e7ef4cf087ffd233ff6490785e6d6cfc6
vadim <bockarevvadim98@gmail.com>
2026-02-11 18:25:44 +0300

    create project & create zustand store

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { memo, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+export const MainPage = memo(() => {
+
+  return (
+    <div style={{height: "100vh", width: "100vw", }}>
+      <nav style={{height: "100%", width: "300px", borderRadius: 13, background: "black"}}>
+        {/* тут листинг проектов */}
+        
+      </nav>
+
+    </div>
+  )
+})
+
+function App() {
+
+  return (
+    <>
+      <nav>
+
+      </nav>
+    </>
+  )
+}
+
+export default App
---
+++ src/stores/projectsStore.ts
@@ -0,0 +1,36 @@
+import { v4 } from "uuid";
+import { create } from "zustand";
+
+
+export type Project = {
+    id: string
+    name: string
+    description: string
+} 
+
+export interface ProjectsStoreInterface {
+    projects: Project[],
+    add: (data: Omit<Project, "id">) => Promise<string>,
+    update: (id: string, payload: Partial<Omit<Project, "id">>) => Promise<void>,
+    delete: (id: string, ) => Promise<void>,
+    filter: (filters: Partial<Project>) => Promise<Project[]>
+}
+
+export const projectsStore = create<ProjectsStoreInterface>((set, get) => ({
+    projects: [],
+
+    add: async (data) => {
+        const newObjId = v4();
+        set(state => ({...state, projects: [...state.projects, {id: newObjId, ...data}]}))
+        return newObjId
+    },
+    update: async (id, payload) => {
+        set(state => ({...state, projects: state.projects.map(proj => proj.id===id ? {...proj, ...payload} : proj)}))
+    },
+    delete: async (id) => {
+        set(state => ({...state, projects: state.projects.filter(project => project.id !== id)}))
+    },
+    filter: async (filters) => {
+        set(state => ({...state, projects: state.projects.filter(project =>)}))
+    } 
+}))
